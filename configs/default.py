9e4cee2ddd5b08fa3de95650303a7eab6cf257fe
Piotr Ambroszczyk <piotr.ambroszczyk@gmail.com>
2019-04-20 20:21:22 +0200

    default config

---
+++ configs/default.py
@@ -0,0 +1,43 @@
+from lrpolicy import LrPolicy as LrP
+
+base_params = {
+    # ----- Convolutions -----
+    'conv_layers': [{'kernel': (11, 41), 'stride': (1, 1), 'num_chan': 1}],
+
+    'frequencies': 100,
+
+    # ----- Recurrent -----
+    'rec_number': 2,
+
+    # ----- FullyConnected -----
+    'fc_layers_sizes': [2048],
+
+    # ----- Others -----
+    'lr_policy': LrP.NO_DECAY,
+    'lr_policy_params': {
+        'lr': 0.0001,
+    },
+
+    'batch_size': 32,
+
+    'batch_norm': False,
+
+    'dropout': 0,
+
+    'epochs': 1,
+    
+    'shuffle_dataset': False,
+
+    'model_saving_epoch': 5
+}
+
+adv_params = {
+    # ----- Spectogram ------
+    'sound_features_size': 100,
+    'sound_time_overlap': 5,
+    'sound_time_length': 20,
+
+    'characters': 29,
+
+    'starting_epoch': 0
+}
