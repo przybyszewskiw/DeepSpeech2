eb539c816c74656e8fb2ffe114368c54767611ab
przybyszewskiw <wp386044@students.mimuw.edu.pl>
2019-04-30 21:31:44 +0200

    DataLoader

--- runner.py
+++ runner.py
@@ -5,15 +5,13 @@ import time
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from dataload import Loader
+import dataload as dl
 from evaluator import eval_single, eval_model
 from model import DeepSpeech
 from scripts.librispeech import LibriSpeech
 import lrpolicy as lrp
 import runpy
 import json  # TODO delete -- only for dict printing
-
-shandom_ruffle = random.shuffle
 
 
 class Runner:
@@ -47,9 +45,7 @@ class Runner:
         if pretrained_model_path is not None:
             self.net.load_state_dict(torch.load(pretrained_model_path))
 
-        self.loader = Loader(num_audio_features=self.adv_params["sound_features_size"],
-                             time_overlap=self.adv_params["sound_time_overlap"],
-                             time_length=self.adv_params["sound_time_length"])
+
 
         self.lr = self.base_params["lr_policy_params"]["lr"]
         self.optimizer = optim.Adam(self.net.parameters(),
@@ -61,86 +57,69 @@ class Runner:
         dataset - list of pairs (track_path, transcript_string)
     """
 
-    def train_epoch(self, dataset, batch_size=8):
+    def train_epoch(self, dataloader):
         self.net.train()
-        tracks_to_merge = []
         total_loss = 0.
         iterations = 0
-        for (i, (track_path, transcript_string)) in enumerate(dataset):
-            tracks_to_merge.append(self.loader.load_tensors(
-                track_path,
-                transcript_string
+
+        for i, (audio, transs, lengths) in enumerate(dataloader):
+            start_time = time.time()
+            self.optimizer.zero_grad()
+
+            audio = audio.to(self.device)
+            output, _ = self.net(audio)
+
+            if self.device != torch.device("cpu"):
+                print("moving net output to CPU")
+                output = output.to("cpu")
+
+            print("Starting criterion calculation")
+            loss = DeepSpeech.criterion(output, transs, lengths)
+            if loss == float('inf'):
+                print(
+                    "WARNING: loss is inf in {}th iteration, omitting track".format(
+                        i), file=sys.stderr)
+                continue
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer_steps += 1
+
+            # for batch_size around 32 in total
+            lr_policy = self.base_params["lr_policy"]
+            lr_policy_params = self.base_params["lr_policy_params"]
+
+            lrp.apply_policy(self.optimizer, self.optimizer_steps, lr_policy, lr_policy_params)
+
+            print("loss in {}th iteration is {}, it took {} seconds".format(
+                i,
+                loss.item(),
+                time.time() - start_time
             ))
-
-            if (i + 1) % batch_size == 0:
-                start_time = time.time()
-                (audio, transs, lengths) = self.loader.merge_into_batch(tracks_to_merge)
-                tracks_to_merge = []
-                self.optimizer.zero_grad()
-
-                audio = audio.to(self.device)
-                output, _ = self.net(audio)
-
-                if self.device != torch.device("cpu"):
-                    print("moving net output to CPU")
-                    output = output.to("cpu")
-
-                print("Starting criterion calculation")
-                loss = DeepSpeech.criterion(output, transs, lengths)
-                if loss == float('inf'):
-                    print(
-                        "WARNING: loss is inf in {}th iteration, track_path:{}, omitting track".format(
-                            i, track_path), file=sys.stderr)
-                    continue
-                loss.backward()
-                self.optimizer.step()
-                self.optimizer_steps += 1
-
-                # for batch_size around 32 in total
-                lr_policy = self.base_params["lr_policy"]
-                lr_policy_params = self.base_params["lr_policy_params"]
-
-                lrp.apply_policy(self.optimizer, self.optimizer_steps, lr_policy, lr_policy_params)
-
-                print("loss in {}th iteration is {}, it took {} seconds".format(
-                    i,
-                    loss.item(),
-                    time.time() - start_time
-                ))
-                total_loss += loss.item()
-                iterations += 1
-                # for some reason output is in the buffer until termination while redirecting to file,
-                # so we have to manually flush
-                sys.stdout.flush()
+            total_loss += loss.item()
+            iterations += 1
+            # for some reason output is in the buffer until termination while redirecting to file,
+            # so we have to manually flush
+            sys.stdout.flush()
 
         print('Total loss in this epoch is {}'.format(total_loss / iterations))
 
-    def test_dataset(self, dataset, batch_size=8):
+    def test_dataset(self, dataloader):
         self.net.eval()
         tracks_to_merge = []
         total_loss = 0.
         iterations = 0
         with torch.no_grad():
-            for (i, (track_path, transcript_string)) in enumerate(dataset):
-                tracks_to_merge.append(self.loader.load_tensors(
-                    track_path,
-                    transcript_string
-                ))
+            for i, (audio, transs, lengths) in enumerate(dataloader):
+                audio = audio.to(self.device)
+                output, _ = self.net(audio)
 
-                if (i + 1) % batch_size == 0:
-                    (audio, transs, lengths) = self.loader.merge_into_batch(tracks_to_merge)
-                    tracks_to_merge = []
+                if self.device != torch.device("cpu"):
+                    output = output.to("cpu")
 
-                    audio = audio.to(self.device)
-                    output, _ = self.net(audio)
+                loss = DeepSpeech.criterion(output, transs, lengths)
 
-                    if self.device != torch.device("cpu"):
-                        output = output.to("cpu")
-
-                    loss = DeepSpeech.criterion(output, transs, lengths)
-
-                    total_loss += loss.item()
-                    iterations += 1
+                total_loss += loss.item()
+                iterations += 1
 
         print('Validation loss is {}'.format(total_loss / iterations))
 
@@ -152,6 +131,16 @@ class Runner:
         shuffle_dataset = self.base_params["shuffle_dataset"]
         sorta_grad = self.base_params['sorta_grad']
 
+        libri_dataset = dl.LibriDataset(dataset,
+                                  num_audio_features=self.adv_params["sound_features_size"],
+                                  time_overlap=self.adv_params["sound_time_overlap"],
+                                  time_length=self.adv_params["sound_time_length"])
+
+        libri_testing_dataset = dl.LibriDataset(testing_dataset,
+                                  num_audio_features=self.adv_params["sound_features_size"],
+                                  time_overlap=self.adv_params["sound_time_overlap"],
+                                  time_length=self.adv_params["sound_time_length"])
+
         self.net.train()
         for epoch in range(starting_epoch, epochs):
             if not os.path.isdir("./models"):
@@ -159,14 +148,19 @@ class Runner:
                 os.makedirs("./models")
             print(epoch)
             start_time = time.time()
-            if shuffle_dataset and not (sorta_grad and epoch == starting_epoch):
-                shandom_ruffle(dataset)
 
-            self.train_epoch(dataset, batch_size=batch_size)
+            libri_dataloader = dl.get_libri_dataloader(libri_dataset, batch_size=batch_size, shuffle=False)
+            if shuffle_dataset and not (sorta_grad and epoch == starting_epoch):
+                libri_dataloader = dl.get_libri_dataloader(libri_dataset, batch_size=batch_size, shuffle=True)
+
+            self.train_epoch(libri_dataloader)
             print('Training {}. epoch took {} seconds'.format(epoch, time.time() - start_time))
 
             if testing_dataset is not None:
-                self.test_dataset(testing_dataset)
+                libri_testing_dataloader = dl.get_libri_dataloader(libri_testing_dataset,
+                                                                   batch_size=batch_size,
+                                                                   shuffle=False)
+                self.test_dataset(libri_testing_dataloader)
 
             if epoch % model_saving_epoch == model_saving_epoch - 1:
                 print('Saving model')
