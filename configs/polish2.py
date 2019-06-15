8dc35a2ebb09eb6864ea658de56cc09fae96cb64
Łukasz Kondraciuk <lukaszkond@gmail.com>
2019-06-15 20:50:31 +0200

    polish2config

---
+++ configs/polish2.py
@@ -0,0 +1,72 @@
+from lrpolicy import LrPolicy as LrP
+from torch.nn.init import xavier_normal_
+import torch.nn as nn
+
+base_params = {
+    # ----- Convolutions -----
+    'conv_layers': [
+        {'kernel': (41, 11), 'stride': (2, 2), 'num_chan': 32},
+        {'kernel': (21, 11), 'stride': (2, 1), 'num_chan': 64},
+        {'kernel': (21, 11), 'stride': (2, 1), 'num_chan': 92},
+    ],
+
+    'frequencies': 160,
+
+    # ----- Recurrent -----
+    'rec_number': 3,
+    'rec_type': 'rnn',
+    'rec_bidirectional': True,
+
+    # ----- FullyConnected -----
+    'fc_layers_sizes': [2048],
+
+    # ----- Others -----
+    'lr_policy': LrP.POLY_DECAY,
+    'lr_policy_params': {
+        'lr': 0.0002,
+        'decay_steps': 1000,
+        'power': 0.5,
+        'min_lr': 0,
+        'max_iter': int(281216 * 50 / 128)  # TODO add correct numbers of iterations
+        # right now {~number of iterations on all-train} * {epochs} / {batch_size}
+    },
+
+    'batch_size': 32,
+
+    'batch_norm': True,
+
+    'dropout': 0.5,
+
+    'epochs': 50,
+
+    'shuffle_dataset': True,
+
+    'model_saving_epoch': 1,
+
+    # starting epoch will be sorted regardless of shuffle_dataset value
+    'sorta_grad': True,
+
+
+    'l2_regularization_scale': 0,
+
+    'weights_initializer': 'xavier_normal',
+    'mixed_precision_opt_level': 'O1'
+}
+
+non_json_params = {
+    'xavier_normal': xavier_normal_,
+}
+
+adv_params = {
+    # ----- Spectogram ------
+    'sound_features_size': 160,
+    'sound_time_overlap': 5,
+    'sound_time_length': 20,
+
+    'characters': 29 + len('ąćęłńóśźż'),
+
+    'starting_epoch': 0,
+
+    'workers': 20,
+}
+
