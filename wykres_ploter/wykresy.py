b0ad1248a1219f4a5f1e1aa7ee56de9f57a2d203
przybyszewskiw <wp386044@students.mimuw.edu.pl>
2019-06-19 12:15:51 +0200

    Changing number of gpus in the plot

--- wykres_ploter/wykresy.py
+++ wykres_ploter/wykresy.py
@@ -20,13 +20,29 @@ def get_plot_data(data, plot_name):
 
 
 filepath = sys.argv[1]
+if len(sys.argv) > 2:
+    gpus = int(sys.argv[2])
+else:
+    gpus = 1
 plot_name = os.path.splitext(filepath)[0]
 file = open(filepath, "r")
 
+sum_tot = 0.0
+sum_val = 0.0
+i_tot = 0
+i_val = 0
 for line in file:
      if re.search("Total", line):
-         total.append(extract_float(line))
+         i_tot += 1
+         sum_tot += extract_float(line)
+         if i_tot % gpus == 0:
+             total.append(sum_tot / gpus)
+             sum_tot = 0.0
      if re.search("Validation", line):
-         validation.append(extract_float(line))
+         i_val += 1
+         sum_val += extract_float(line)
+         if i_val % gpus == 0:
+             validation.append(sum_val / gpus)
+             sum_val = 0.0
 
 print(get_plot_data(zip(total, validation), plot_name))
